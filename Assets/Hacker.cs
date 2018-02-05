8aa66c8eacc6c0852503097ce74ccaaf728af684
Richard Plant <repairist@gmail.com>
2018-02-05 11:25:57 +0000

    Add ascii art win screens

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -1,9 +1,7 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class Hacker : MonoBehaviour {
+public class Hacker : MonoBehaviour
+{
 
     // Config vars
     enum Screen { Menu, Password, Win };
@@ -16,18 +14,58 @@ public class Hacker : MonoBehaviour {
     Screen currentScreen = Screen.Menu;
     string currentPassword;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
         string greeting = "Welcome User:[Rich]";
         MainMenu(greeting);
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
 
-	}
+    }
+
+    // Display main menu screen
+    void MainMenu(string greeting = "")
+    {
+        currentScreen = Screen.Menu;
+        Terminal.ClearScreen();
+        if (greeting.Length > 0)
+        {
+            Terminal.WriteLine(greeting);
+        }
+        Terminal.WriteLine("Do you want to play a game?");
+        Terminal.WriteLine("Not global thermonuclear war, promise.");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("Enter 1 to snoop on MI5");
+        Terminal.WriteLine("Enter 2 to gather data on the FSB");
+        Terminal.WriteLine("Enter 3 to penetrate the NSA network");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("Your choice: ");
+    }
+
+    // Called when level is selected on main menu
+    void StartGame()
+    {
+        currentScreen = Screen.Password;
+        currentPassword = passwords[level - 1, Random.Range(0, passwords.GetLength(1))];
+        Terminal.ClearScreen();
+        Terminal.WriteLine("Thanks for selecting access level " + level);
+        Terminal.WriteLine("Please enter your password.");
+        Terminal.WriteLine("Type menu to return to level select");
+        Terminal.WriteLine("");
+        Terminal.WriteLine("Waiting for your input: ");
+    }
+
+    // Called when correct password entered
+    void Winner()
+    {
+        currentScreen = Screen.Win;
+        Terminal.ClearScreen();
+        WinHandler();
+    }
 
     // Called when user confirms input - pass input to handlers
     void OnUserInput(string input)
@@ -54,23 +92,6 @@ public class Hacker : MonoBehaviour {
         }
     }
 
-    // Handle inputs on password guess screen
-    void PassHandler(string input)
-    {
-        if (input.ToLower() == currentPassword.ToLower())
-        {
-            Terminal.WriteLine("");
-            Terminal.WriteLine("Excellent!");
-            Terminal.WriteLine("Those security techs will regret");
-            Terminal.WriteLine("they were ever born!");
-        } else
-        {
-            Terminal.WriteLine("");
-            Terminal.WriteLine("Oh well, can't all be winners.");
-            Terminal.WriteLine("Go on, try again:");
-        }
-    }
-
     // Handle inputs on main menu
     void MenuHandler(string input)
     {
@@ -93,36 +114,66 @@ public class Hacker : MonoBehaviour {
         }
     }
 
-
-    // Called when level is selected on main menu
-    void StartGame()
+    // Handle inputs on password guess screen
+    void PassHandler(string input)
     {
-        currentScreen = Screen.Password;
-        currentPassword = passwords[level - 1, UnityEngine.Random.Range(0, passwords.GetLength(1))];
-        Terminal.ClearScreen();
-        Terminal.WriteLine("Thanks for selecting access level " + level);
-        Terminal.WriteLine("Please enter your password.");
-        Terminal.WriteLine("Type menu to return to level select");
-        Terminal.WriteLine("");
-        Terminal.WriteLine("Waiting for your input: ");
+        if (input.ToLower() == currentPassword.ToLower())
+        {
+            Winner();
+        }
+        else
+        {
+            Terminal.WriteLine("");
+            Terminal.WriteLine("Oh well, can't all be winners.");
+            Terminal.WriteLine("Go on, try again:");
+        }
     }
 
-    // Display main menu screen
-    void MainMenu (string greeting = "")
+    // Handle logic for win screen
+    void WinHandler()
     {
-        currentScreen = Screen.Menu;
-        Terminal.ClearScreen();
-        if (greeting.Length > 0)
+        switch(level)
         {
-            Terminal.WriteLine(greeting);
+            case 1:
+                Terminal.WriteLine("Excellent!");
+                Terminal.WriteLine("Those tea-drinkers will rue the day.");
+                Terminal.WriteLine(@"
+           ( (
+            ) )
+          ........
+          |      |]
+          \      /
+           `----'
+                ");
+                break;
+            case 2:
+                Terminal.WriteLine("Khorosho!");
+                Terminal.WriteLine("Cold War? More like old snore.");
+                Terminal.WriteLine(@"
+                -..
+            //     ))
+           //\  . _.'
+          '/ \\//~
+              \\
+             //\\
+             '  '
+                ");
+                break;
+            case 3:
+                Terminal.WriteLine("Tubular!");
+                Terminal.WriteLine("Turns out aliens are totally real.");
+                Terminal.WriteLine(@"
+                           .-.
+            .-''`''-.    |(@ @)
+         _/`oOoOoOoOo`\_ \ \-/
+        '.-=-=-=-=-=-=-.' \/ \
+          `-=.=-.-=.=-'    \ /\
+             ^  ^  ^       _H_ \
+                ");
+                break;
+            default:
+                Debug.LogError("Error in WinHandler");
+                break;
         }
-        Terminal.WriteLine("Do you want to play a game?");
-        Terminal.WriteLine("Not global thermonuclear war, promise.");
-        Terminal.WriteLine("");
-        Terminal.WriteLine("Enter 1 to snoop on MI5");
-        Terminal.WriteLine("Enter 2 to gather data on the FSB");
-        Terminal.WriteLine("Enter 3 to penetrate the NSA network");
-        Terminal.WriteLine("");
-        Terminal.WriteLine("Your choice: ");
     }
 }
